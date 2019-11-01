b3ae0e854abe3616bee23193caccacc77677a8cf
perfectlylogical <perfectlylogical@users.noreply.github.com>
2019-11-01 14:31:01 -0400

    formats changed slightly updated to get it working again

--- cloudpassage_has_ubuntu_patched_cve.py
+++ cloudpassage_has_ubuntu_patched_cve.py
@@ -15,7 +15,7 @@ for issue in issue_list:
   if issue['issue_type'] == 'sva':
     if issue['package_name'] != all_cves:
       all_cves[issue['package_name']] = []
-    for cve in issue['cves']:
+    for cve in issue['cve_ids']:
       all_cves[issue['package_name']].append(cve)
 
 for package in all_cves:
@@ -25,9 +25,10 @@ for package in all_cves:
     url = 'https://people.canonical.com/~ubuntu-security/cve/{0}/{1}.html'.format(cve.split("-")[1],cve)
     resp = requests.get(url)
     page = BeautifulSoup(resp.content, 'html.parser')
-    table = page.find(lambda tag: tag.name=='table')
-    for row in table.findAll("tr"):
-      # Check if the package is patched in Ubuntu 16.04
-      if "16.04" in str(row):
-        columns = row.find_all('td')
-        print("{0},{1},{2}".format(package,cve,columns[1].find_next('span').get_text()))
+    tables = page.findAll(lambda tag: tag.name=='table')
+    for table in tables
+      for row in table.findAll("tr"):
+        # Check if the package is patched in Ubuntu 16.04
+        if "16.04" in str(row):
+          columns = row.find_all('td')
+          print("{0},{1},{2}".format(package,cve,columns[1].find_next('span').get_text()))
